c03772fb470d8ef47045aaae52ca66d8167e80f4
SkJaan110 <106261813+SkJaan110@users.noreply.github.com>
2025-09-27 14:05:45 +0500

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBTmNU41Z0hh9Rlk3F1OVzlweKEAJ7cIHY",
+  authDomain: "cricket-scorers-cd973.firebaseapp.com",
+  databaseURL: "https://cricket-scorers-cd973-default-rtdb.firebaseio.com",
+  projectId: "cricket-scorers-cd973",
+  storageBucket: "cricket-scorers-cd973.firebasestorage.app",
+  messagingSenderId: "709327604946",
+  appId: "1:709327604946:web:3ecbdfe19a7a3a36a6fc6e"
+};
+firebase.initializeApp(firebaseConfig);
